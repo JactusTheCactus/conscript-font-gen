1d7a5c69f9c30d5d9a08ac12f7e314a76a569e45
JactusTheCactus <threateninglyamusing@gmail.com>
2025-07-15 07:10:57 -0700

    Update script.py

--- script.py
+++ script.py
@@ -42,34 +42,26 @@ liga.append("feature liga {")
 if True:
 	for l in letters:
 		liga.append(f"\tsub {l.lower()} by {l};")
-	liga.append("")
 	for l in range(len(punctuation)):
 		liga.append(f"\tsub {lvn[l]} special by {lvy[l]};")
-	liga.append("")
 	for p in punctuation:
 		for v in vowels:
 			liga.append(f"\tsub {p} {v}' by X {v};")
-	liga.append("")
 	for non in nonV:
 		liga.append(f"\tsub start {non} by {non};")
-	liga.append("")
 	for v1 in vowels:
 		for v2 in vowels:
 			liga.append(f"\tsub {v1} {v2}' by X {v2};")
-	liga.append("")
 	for c in consonants:
 		for v in vowels:
 			liga.append(f"\tsub {c} {v} by {c}_{v};")
 			liga.append(f"\tsub {c} {v} emphasis by {c}_{v}_emphasis;")
-	liga.append("")
 	for p1 in punctuation:
 		for p2 in punctuation:
 			liga.append(f"\tsub {p1} {p2} by {p1};")
-	liga.append("")
 	for p in punctuation:
 		liga.append(f"\tsub {p} space by {p};")
 		liga.append(f"\tsub space {p} by {p};")
-	liga.append("")
 	liga.append(f"\tsub period period period by ellipsis;")
 liga.append("} liga;")
 liga = "\n".join(liga)
