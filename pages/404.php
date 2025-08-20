b22bef79307354a68811a1ef880ab60bffb7091b
JactusTheCactus <threateninglyamusing@gmail.com>
2025-08-20 02:35:28 -0700

    Update 404.php

--- pages/404.php
+++ pages/404.php
@@ -4,10 +4,8 @@
 <link href="style.css" rel="stylesheet">
 </head>
 <body>
-<div style="text-align:center;">
 <h1>Oops!</h1>
 <h2>I can't find that page!<h2/>
 <h3>;-;</h3>
-</div>
 </body>
 </html>
