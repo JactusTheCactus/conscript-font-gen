00471734fc3827ad7a95daf1842c071167111d29
JactusTheCactus <threateninglyamusing@gmail.com>
2025-08-20 02:45:57 -0700

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -1,20 +1,20 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-	<link href="style.css" rel="stylesheet">
-	<script src="fmt.js"></script>
-</head>
-
-<body>
-	<h1>Scripts</h1>
-	<ul><?php $scripts = array("AbugidaR", "AlphabetD");
-		foreach ($scripts as $s) {
-			echo "<li>$s<ul>" .
-				"<li><a href='$s/ipa.html'>Phonemes</a></li>" .
-				"<li><a href='$s/ivlivs-caesar.html'>Example</a></li>" .
-				"</ul></li>";
-		}; ?></ul>
-</body>
-
-</html>
+<?php
+echo "<!DOCTYPE html>".
+"<html>".
+"<head>".
+"<link href='style.css' rel='stylesheet'>".
+"<script src='fmt.js'></script>".
+"</head>".
+"<body>".
+"<h1>Scripts</h1>".
+"<ul>";
+foreach (["AbugidaR", "AlphabetD"] as $s) {
+echo "<li>$s<ul>" .
+"<li><a href='$s/ipa.html'>Phonemes</a></li>" .
+"<li><a href='$s/ivlivs-caesar.html'>Example</a></li>" .
+"</ul></li>";
+};
+echo "</ul>".
+"</body>".
+"</html>";
+?>
