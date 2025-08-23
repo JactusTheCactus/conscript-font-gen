96f320b8bcc2524583f10e666c926abd1bf02cd4
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-08-22 23:17:55 -0700

    -

--- VUE/src/views/AbR/IPA.vue
+++ VUE/src/views/AbR/IPA.vue
@@ -1,5 +1,340 @@
+<style scoped>
+tr * {
+	text-align: center;
+}
+</style>
 <template>
 	<div id="app">
-		<pre><code>$ IPA</code></pre>
+		<section>
+			<table>
+				<tr>
+					<th>AbugidaR</th>
+				</tr>
+			</table>
+		</section>
+		<br />
+		<br />
+		<section>
+			<table>
+				<tr>
+					<th colspan=5>Consonants</th>
+				</tr>
+				<tr>
+					<td>
+						B
+						<br />
+						<span class="AbugidaR">B</span>
+						<br />
+						<span class="ipa">b</span>
+					</td>
+					<td>
+						C
+						<br />
+						<span class="AbugidaR">C</span>
+						<br />
+						<span class="ipa">t&#865;&#x0283;</span>
+					</td>
+					<td>
+						D
+						<br />
+						<span class="AbugidaR">D</span>
+						<br />
+						<span class="ipa">d</span>
+					</td>
+					<td>
+						&ETH;
+						<br />
+						<span class="AbugidaR">D;</span>
+						<br />
+						<span class="ipa">ð</span>
+					</td>
+					<td>
+						F
+						<br />
+						<span class="AbugidaR">F</span>
+						<br />
+						<span class="ipa">f</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						G
+						<br />
+						<span class="AbugidaR">G</span>
+						<br />
+						<span class="ipa">g</span>
+					</td>
+					<td>
+						H
+						<br />
+						<span class="AbugidaR">H</span>
+						<br />
+						<span class="ipa">h</span>
+					</td>
+					<td>
+						J
+						<br />
+						<span class="AbugidaR">J</span>
+						<br />
+						<span class="ipa">d&#865;&#x0292;</span>
+					</td>
+					<td>
+						K
+						<br />
+						<span class="AbugidaR">K</span>
+						<br />
+						<span class="ipa">k</span>
+					</td>
+					<td>
+						L
+						<br />
+						<span class="AbugidaR">L</span>
+						<br />
+						<span class="ipa">l</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						M
+						<br />
+						<span class="AbugidaR">M</span>
+						<br />
+						<span class="ipa">m</span>
+					</td>
+					<td>
+						N
+						<br />
+						<span class="AbugidaR">N</span>
+						<br />
+						<span class="ipa">n</span>
+					</td>
+					<td>
+						&ENG;
+						<br />
+						<span class="AbugidaR">N;</span>
+						<br />
+						<span class="ipa">&eng;</span>
+					</td>
+					<td>
+						P
+						<br />
+						<span class="AbugidaR">P</span>
+						<br />
+						<span class="ipa">p</span>
+					</td>
+					<td>
+						R
+						<br />
+						<span class="AbugidaR">R</span>
+						<br />
+						<span class="ipa">&#x0279;</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						S
+						<br />
+						<span class="AbugidaR">S</span>
+						<br />
+						<span class="ipa">s</span>
+					</td>
+					<td>
+						&Sacute;
+						<br />
+						<span class="AbugidaR">S;</span>
+						<br />
+						<span class="ipa">&#x0283;</span>
+					</td>
+					<td>
+						T
+						<br />
+						<span class="AbugidaR">T</span>
+						<br />
+						<span class="ipa">t</span>
+					</td>
+					<td>
+						&THORN;
+						<br />
+						<span class="AbugidaR">T;</span>
+						<br />
+						<span class="ipa">&#x03B8;</span>
+					</td>
+					<td>
+						V
+						<br />
+						<span class="AbugidaR">V</span>
+						<br />
+						<span class="ipa">v</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						W
+						<br />
+						<span class="AbugidaR">W</span>
+						<br />
+						<span class="ipa">w</span>
+					</td>
+					<td>
+						Y
+						<br />
+						<span class="AbugidaR">Y</span>
+						<br />
+						<span class="ipa">j</span>
+					</td>
+					<td>
+						Z
+						<br />
+						<span class="AbugidaR">Z</span>
+						<br />
+						<span class="ipa">z</span>
+					</td>
+					<td>
+						&Zacute;
+						<br />
+						<span class="AbugidaR">Z;</span>
+						<br />
+						<span class="ipa">&#x0292;</span>
+					</td>
+				</tr>
+			</table>
+		</section>
+		<br />
+		<section>
+			<table>
+				<tr>
+					<th colspan=5>Vowels</th>
+				</tr>
+				<tr>
+					<td>
+						A
+						<br />
+						<span class="AbugidaR">A</span>
+						<br />
+						<span class="ipa">&#x0252;</span>
+					</td>
+					<td>
+						E
+						<br />
+						<span class="AbugidaR">E</span>
+						<br />
+						<span class="ipa">&#x025b;</span>
+					</td>
+					<td>
+						&Eacute;
+						<br />
+						<span class="AbugidaR">E;</span>
+						<br />
+						<span class="ipa">e</span>
+					</td>
+					<td>
+						I
+						<br />
+						<span class="AbugidaR">I</span>
+						<br />
+						<span class="ipa">&#x026a;</span>
+					</td>
+					<td>
+						&Iacute;
+						<br />
+						<span class="AbugidaR">I;</span>
+						<br />
+						<span class="ipa">i</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						O
+						<br />
+						<span class="AbugidaR">O</span>
+						<br />
+						<span class="ipa">&#x0254;</span>
+					</td>
+					<td>
+						&Oacute;
+						<br />
+						<span class="AbugidaR">O;</span>
+						<br />
+						<span class="ipa">o</span>
+					</td>
+					<td>
+						U
+						<br />
+						<span class="AbugidaR">U</span>
+						<br />
+						<span class="ipa">&#x0259;</span>
+					</td>
+					<td>
+						&Uacute;
+						<br />
+						<span class="AbugidaR">U;</span>
+						<br />
+						<span class="ipa">u</span>
+					</td>
+				</tr>
+			</table>
+		</section>
+		<br />
+		<section>
+			<table>
+				<tr>
+					<th colspan=5>Punctuation</th>
+				</tr>
+				<tr>
+					<td>
+						&#x2423;
+						<br />
+						<pre class="AbugidaR pre">&#x20;</pre>
+						<i>[SPACE]</i>
+					</td>
+					<td>
+						,
+						<br />
+						<span class="AbugidaR">,</span>
+						<br />
+						<i>[COMMA]</i>
+					</td>
+					<td>
+						?
+						<br />
+						<span class="AbugidaR">?</span>
+						<br />
+						<i>[QUESTION]</i>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						.
+						<br />
+						<span class="AbugidaR">.</span>
+						<br />
+						<i>[STOP]</i>
+					</td>
+					<td>
+						...
+						<br />
+						<span class="AbugidaR">...</span>
+						<br />
+						<i>[ELLIPSIS]</i>
+					</td>
+					<td>
+						&ndash;
+						<br />
+						<span class="AbugidaR">-</span>
+						<br />
+						<i>[DASH]</i>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						'
+						<br />
+						<span class="AbugidaR">'</span>
+						<br />
+						<i>[EMPHASIS]</i>
+					</td>
+				</tr>
+			</table>
+		</section>
 	</div>
 </template>
--- VUE/src/views/AlD/IPA.vue
+++ VUE/src/views/AlD/IPA.vue
@@ -1,5 +1,339 @@
+<style scoped>
+tr * {
+	text-align: center;
+}
+</style>
 <template>
 	<div id="app">
-		<pre><code>$ IPA</code></pre>
+		<section>
+			<table>
+				<tr>
+					<th>AlphabetD</th>
+				</tr>
+			</table>
+		</section>
+		<br />
+		<br />
+		<section>
+			<table>
+				<tr>
+					<th colspan=5>Consonants</th>
+				</tr>
+				<tr>
+					<td>
+						B
+						<br />
+						<span class="AlphabetD">B</span>
+						<br />
+						<span class="ipa">b</span>
+					</td>
+					<td>
+						C
+						<br />
+						<span class="AlphabetD">C</span>
+						<br />
+						<span class="ipa">t&#865;&#x0283;</span>
+					</td>
+					<td>
+						D
+						<br />
+						<span class="AlphabetD">D</span>
+						<br />
+						<span class="ipa">d</span>
+					</td>
+					<td>
+						&ETH;
+						<br />
+						<span class="AlphabetD">D;</span>
+						<br />
+						<span class="ipa">ð</span>
+					</td>
+					<td>
+						F
+						<br />
+						<span class="AlphabetD">F</span>
+						<br />
+						<span class="ipa">f</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						G
+						<br />
+						<span class="AlphabetD">G</span>
+						<br />
+						<span class="ipa">g</span>
+					</td>
+					<td>
+						H
+						<br />
+						<span class="AlphabetD">H</span>
+						<br />
+						<span class="ipa">h</span>
+					</td>
+					<td>
+						J
+						<br />
+						<span class="AlphabetD">J</span>
+						<br />
+						<span class="ipa">d&#865;&#x0292;</span>
+					</td>
+					<td>
+						K
+						<br />
+						<span class="AlphabetD">K</span>
+						<br />
+						<span class="ipa">k</span>
+					</td>
+					<td>
+						L
+						<br />
+						<span class="AlphabetD">L</span>
+						<br />
+						<span class="ipa">l</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						M
+						<br />
+						<span class="AlphabetD">M</span>
+						<br />
+						<span class="ipa">m</span>
+					</td>
+					<td>
+						N
+						<br />
+						<span class="AlphabetD">N</span>
+						<br />
+						<span class="ipa">n</span>
+					</td>
+					<td>
+						&ENG;
+						<br />
+						<span class="AlphabetD">N;</span>
+						<br />
+						<span class="ipa">&eng;</span>
+					</td>
+					<td>
+						P
+						<br />
+						<span class="AlphabetD">P</span>
+						<br />
+						<span class="ipa">p</span>
+					</td>
+					<td>
+						R
+						<br />
+						<span class="AlphabetD">R</span>
+						<br />
+						<span class="ipa">&#x0279;</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						S
+						<br />
+						<span class="AlphabetD">S</span>
+						<br />
+						<span class="ipa">s</span>
+					</td>
+					<td>
+						&Sacute;
+						<br />
+						<span class="AlphabetD">S;</span>
+						<br />
+						<span class="ipa">&#x0283;</span>
+					</td>
+					<td>
+						T
+						<br />
+						<span class="AlphabetD">T</span>
+						<br />
+						<span class="ipa">t</span>
+					</td>
+					<td>
+						&THORN;
+						<br />
+						<span class="AlphabetD">T;</span>
+						<br />
+						<span class="ipa">&#x03B8;</span>
+					</td>
+					<td>
+						V
+						<br />
+						<span class="AlphabetD">V</span>
+						<br />
+						<span class="ipa">v</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						W
+						<br />
+						<span class="AlphabetD">W</span>
+						<br />
+						<span class="ipa">w</span>
+					</td>
+					<td>
+						Y
+						<br />
+						<span class="AlphabetD">Y</span>
+						<br />
+						<span class="ipa">j</span>
+					</td>
+					<td>
+						Z
+						<br />
+						<span class="AlphabetD">Z</span>
+						<br />
+						<span class="ipa">z</span>
+					</td>
+					<td>
+						&Zacute;
+						<br />
+						<span class="AlphabetD">Z;</span>
+						<br />
+						<span class="ipa">&#x0292;</span>
+					</td>
+				</tr>
+			</table>
+		</section>
+		<br />
+		<section>
+			<table>
+				<tr>
+					<th colspan=5>Vowels</th>
+				</tr>
+				<tr>
+					<td>
+						A
+						<br />
+						<span class="AlphabetD">A</span>
+						<br />
+						<span class="ipa">&#x0252;</span>
+					</td>
+					<td>
+						E
+						<br />
+						<span class="AlphabetD">E</span>
+						<br />
+						<span class="ipa">&#x025b;</span>
+					</td>
+					<td>
+						&Eacute;
+						<br />
+						<span class="AlphabetD">E;</span>
+						<br />
+						<span class="ipa">e</span>
+					</td>
+					<td>
+						I
+						<br />
+						<span class="AlphabetD">I</span>
+						<br />
+						<span class="ipa">&#x026a;</span>
+					</td>
+					<td>
+						&Iacute;
+						<br />
+						<span class="AlphabetD">I;</span>
+						<br />
+						<span class="ipa">i</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						O
+						<br />
+						<span class="AlphabetD">O</span>
+						<br />
+						<span class="ipa">&#x0254;</span>
+					</td>
+					<td>
+						&Oacute;
+						<br />
+						<span class="AlphabetD">O;</span>
+						<br />
+						<span class="ipa">o</span>
+					</td>
+					<td>
+						U
+						<br />
+						<span class="AlphabetD">U</span>
+						<br />
+						<span class="ipa">&#x0259;</span>
+					</td>
+					<td>
+						&Uacute;
+						<br />
+						<span class="AlphabetD">U;</span>
+						<br />
+						<span class="ipa">u</span>
+					</td>
+					<td>
+						&#x0170;
+						<br />
+						<span class="AlphabetD">U;;</span>
+						<br />
+						<span class="ipa">&#x028a;</span>
+					</td>
+				</tr>
+			</table>
+		</section>
+		<br />
+		<section>
+			<table>
+				<tr>
+					<th colspan=5>Punctuation</th>
+				</tr>
+				<tr>
+					<td>
+						&#x2423;
+						<br />
+						<span class="AlphabetD pre">&#x20;</span>
+						<br/>
+						<i>[SPACE]</i>
+					</td>
+					<td>
+						,
+						<br />
+						<span class="AlphabetD">,</span>
+						<br />
+						<i>[COMMA]</i>
+					</td>
+					<td>
+						?
+						<br />
+						<span class="AlphabetD">?</span>
+						<br />
+						<i>[QUESTION]</i>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						.
+						<br />
+						<span class="AlphabetD">.</span>
+						<br />
+						<i>[STOP]</i>
+					</td>
+					<td>
+						...
+						<br />
+						<span class="AlphabetD">...</span>
+						<br />
+						<i>[ELLIPSIS]</i>
+					</td>
+					<td>
+						&ndash;
+						<br />
+						<span class="AlphabetD">-</span>
+						<br />
+						<i>[DASH]</i>
+					</td>
+				</tr>
+			</table>
+		</section>
 	</div>
 </template>
